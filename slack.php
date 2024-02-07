97c26c4647dc9f3b47d13e7aff335303a971e97e
Khush1694 <132439373+Khush1694@users.noreply.github.com>
2024-02-07 18:32:03 +0530

    Update slack.php

--- slack.php
+++ slack.php
@@ -1,5 +1,3 @@
 private readonly LoggerInterface $logger,        
 private readonly UserDeletedService $userDeletedService,
 private readonly PlatformManagerInterface $platformManager,
-private readonly SlackClientInterface $slackClient,
-private readonly SlackClientInterface $slackClient
